4b7a39627769fe736399546d9e8bc40c19f23054
Maksim <maxloda2013@yandex.ru>
2024-05-04 13:47:14 +0500

    delete CountChanger

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -1,8 +1,14 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class Counter : MonoBehaviour
 {
+    [SerializeField] private float _changeDelay;
+    [SerializeField] private PlayerInput _playerInput;
+
+    private Coroutine _coroutine;
+
     public event Action<int> CountChanged;
 
     public int Count { get; private set; }
@@ -12,9 +18,44 @@ public class Counter : MonoBehaviour
         Count = 0;
     }
 
+    private void OnEnable()
+    {
+        _playerInput.Clicked += OnClicked;
+    }
+
+    private void OnDisable()
+    {
+        _playerInput.Clicked -= OnClicked;
+    }
+
     public void Add()
     {
         Count++;
         CountChanged?.Invoke(Count);
+    }
+
+    private IEnumerator ChangeCountRoutine()
+    {
+        WaitForSeconds wait = new WaitForSeconds(_changeDelay);
+
+        while (enabled)
+        {
+            Add();
+
+            yield return wait;
+        }
+    }
+
+    private void OnClicked()
+    {
+        if (_coroutine == null)
+        {
+            _coroutine = StartCoroutine(ChangeCountRoutine());
+        }
+        else
+        {
+            StopCoroutine(_coroutine);
+            _coroutine = null;
+        }
     }
 }
